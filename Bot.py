8f4506d46d377275b08808e38cc577597ab33f3e
E1ast <arturserikov3986@gmail.com>
2023-12-14 22:01:41 +0300

    feat: Добавлено автоматическое скачивание изображения

--- Bot.py
+++ Bot.py
@@ -2,7 +2,7 @@
 #Contact Email: artueserikov3986@gmail.com
 
 #Импорт библиотек:
-import telebot, configparser, requests
+import telebot, configparser, requests, wget, os
 from bs4 import BeautifulSoup
 from PIL import Image
 
@@ -11,17 +11,18 @@ config = configparser.ConfigParser()
 config.read('Config.ini')
 token = config["Private"]["token"] #Присваиваем токен переменной
 url = config["Private"]["url"] #Присваиваем url переменной
-
+dir = os.getcwd() #Узнаем текущую директорию
+print(f"\nТекущая директория: {dir}")
 
 #Переменные:
 run = True
 
 
-#Функция ниже предназначена для парсинга сайта
+#Функция ниже предназначена для парсинга сайта и скачивания изображения
 #Она ищет все ссылки и мы выбираем нужные из них
 #Данные ссылки используются для скачивания изображения с сайта
 
-@staticmethod #Функция парсинга
+@staticmethod
 def Pars():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
@@ -33,21 +34,39 @@ def Pars():
         links_all = link.get('src')
         Counter += 1 #Счетчик
         if Counter == 4: #Принтим 4-ый элемент
-            img = (link.get('src')) #(link.get('src')) - фильтр
-            print (img)
-    return(img)
+            img_url = (link.get('src')) #(link.get('src')) - фильтр
+            print (f"Текущая активная ссылка: {img_url}")
+    if (os.path.exists('schedule.jpg')) == False:
+        wget.download(img_url, 'schedule.jpg')
+    else:
+        print(f"Если вы хотите обновить изображение, то удалите schedule.jpg и перезапустите программу")
 
 Pars() #Вызов парсера
 
 
 #Код самого бота:
 bot = telebot.TeleBot(token) #Создаем бота
+print("\n\nБот запущен!\n\nСобытия:")
 
 #Ответ на /start
 @bot.message_handler(commands=['start']) #Условия запуска функции
 def start(message): #Функция, которая будем выполнена при условии выше
     bot.send_message(message.chat.id, 'Привет!')
-    print("Старт")
+    print("Кто-то использовал /start")
+
+#Ответ на /расписание
+@bot.message_handler(commands=['расписание']) #Условия запуска функции
+def schedule(message): #Функция, которая будем выполнена при условии выше
+    schedule = open("schedule.jpg", "rb")
+    bot.send_message(message.chat.id, "Актуальное расписание:")
+    bot.send_photo(message.chat.id, schedule)
+    print("Кто-то решил узнать расписание")
+
+#Ответ на /start
+@bot.message_handler(commands=['start']) #Условия запуска функции
+def start(message): #Функция, которая будем выполнена при условии выше
+    bot.send_message(message.chat.id, 'Привет!')
+    print("Кто-то использовал /start")
 
 #Обработка обычного текста
 @bot.message_handler()
@@ -55,14 +74,17 @@ def akk_message(message):
     #Отправка изображения по текстовому запросу:
     if message.text.lower() == "расписание" or "распорядок" or "пары" or "занятия":
         schedule = open("schedule.jpg", "rb")
-        bot.send_message(message.chat.id, "Расписание на 12 декабря")
+        bot.send_message(message.chat.id, "Актуальное расписание:")
         bot.send_photo(message.chat.id, schedule)
+        print("Кто-то решил узнать расписание")
 
     #Отправка ссылки на сайт по текстовому запросу:
     elif message.text.lower() == "сайт" or "site" or "website" or "вебсайт":
-        bot.send_message(message.chat.id, "cайт")
+        bot.send_message(message.chat.id, "http://simfpolyteh.ru/raspisanie/")
+        print("Кто-то решил зайти на сайт колледжа")
     else:
-        pass
+        bot.send_message(message.chat.id, "Такой команды нет :/")
+        print("Кто-то ввел команду неправильно")
 
 
 
